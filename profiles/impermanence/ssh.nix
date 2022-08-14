ea0ba823aef90ec0c01f80e019bdbec4fa17d72c
Maksymilian Jodlowski <maksymilian.jodlowski@gmail.com>
2022-08-14 14:00:02 +0000

    Add host-gate rsa key
    Fix
    
    Fix
    
    Fix
    
    Fix

--- profiles/impermanence/ssh.nix
+++ profiles/impermanence/ssh.nix
@@ -1,15 +1,14 @@
 { ... }:
 {
-  services.openssh.hostKeys = {
+  services.openssh.hostKeys = [
     {
-    path = "/nix/persist/etc/ssh/ssh_host_ed25519_key";
-    type = "ed25519";
-  },
-  {
-  path = "/persist/ssh/ssh_host_rsa_key";
-  type = "rsa";
-  bits = 4096;
-}
-
-}
+      path = "/nix/persist/etc/ssh/ssh_host_ed25519_key";
+      type = "ed25519";
+    }
+    {
+      path = "/nix/persist/etc/ssh/ssh_host_rsa_key";
+      type = "rsa";
+      bits = 4096;
+    }
+  ];
 }
--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -1,8 +1,7 @@
 let
   # set ssh public keys here for your system and user
-  system = "";
-  user = "";
-  allKeys = [ system user ];
+  host-gate = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQDJAg7g73+h8cdV0kyZZxn04wYY+agFFYJoXvDVWs7e+De6Dyd+wEbdsV7N6lW6+HXUmUDLhPd+b8n+AtlsdUKj28wQfm/XB4Ze5+kGJxSHQsJmrH75Tv4O9H2CaUnQMJFZivAEeNLVdrD2hESkPUdIgfCVHW9O3iquMAMJbsQPWvoZKyO+Iv8zPzilM2mhZfaUt5fQvr4HxujNnboiWMDE+/0ZUDcNy75xcFoJvfogAHRMjmEdoWs3snZg8Od0tqpFlKqWemWX0iyP7hroMXmes2y/s5i8zwSrtaZGU0YA2ZMmKqh/L/3b4N29g7UsjHBzt07n+FkvHyZHJtvx9t2uumSajl8ufR33ArtOSuI8fzpiOsEutj9ATdx6vQdrGqUqBREZ1vxYa1IdOvfox9dDXcR1sEVn/SuYuSwW2Y7rQT7tcMxUBHhhBjdlioQEbFY5Lk2HC42veAiKV6Wfj6tu3/4VQ3nDp2isu0nJEvQywVZrg0mNxx+NleTtYjnZFu6dZnUJTNhEYSv321gda6eEqWELymq5WLx3Cx0a3Va4RzRH9Z1/VsZH+oyguBOqYwOFDDTj/aXYEEFm1xnWmWJ4h/K/1PPWQZuZnX7lZ+yhDx/qTofGq3DqruyTATDRzBI0+2UETyG3MEntCIElV4ERi+5mOwktcthODyAV6T8Oew==";
+  allKeys = [ host-gate ];
 in
 {
   "secret.age".publicKeys = allKeys;
