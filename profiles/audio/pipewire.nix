d4fb7b2baa88d3cdcbdea1663cb52c647a6cad99
Maksymilian Jodlowski <maksymilian.jodlowski@gmail.com>
2022-08-14 21:17:51 +0000

    audio profile

---
+++ profiles/audio/pipewire.nix
@@ -0,0 +1,13 @@
+{ pkgs, ... }:
+
+{
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    pulse.enable = true;
+  };
+
+  environment.systemPackages = with pkgs; [
+    pavucontrol
+  ];
+}
