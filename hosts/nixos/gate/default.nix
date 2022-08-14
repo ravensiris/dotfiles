e55a96f7f7e154abc13afdd6b553289e6d5a8f91
Maksymilian Jodlowski <maksymilian.jodlowski@gmail.com>
2022-08-14 19:39:22 +0000

    Fix

--- hosts/nixos/gate/default.nix
+++ hosts/nixos/gate/default.nix
@@ -23,135 +23,136 @@
     pavucontrol
     ddccontrol
     (pkgs.nerdfonts.override {
-      fonts = [ "VictorMono" ] })
+      fonts = [ "VictorMono" ];
+    })
+  ];
+
+
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    pulse.enable = true;
+  };
+
+  services.ddccontrol.enable = true;
+
+  virtualisation.libvirtd.enable = true;
+  virtualisation.libvirtd.qemu.ovmf.enable = true;
+  virtualisation.libvirtd.qemu.verbatimConfig = ''
+    nvram = [
+      "${pkgs.OVMF}/FV/OVMF.fd:${pkgs.OVMF}/FV/OVMF_VARS.fd"
+    ]
+    namespaces = []
+    user = "+1000"
+  '';
+
+
+  services.xserver.videoDrivers = [ "amdgpu" ];
+
+  hardware.enableRedistributableFirmware = true;
+  hardware.opengl.enable = true;
+  hardware.opengl.driSupport = true;
+
+  # programs.sway.enable = true;
+
+  networking.useDHCP = lib.mkDefault true;
+
+  # hardware.cpu.amd.updateMicrocode = config.hardware.enableRedistributableFirmware;
+
+  # high-resolution display
+  hardware.video.hidpi.enable = true;
+  environment.pathsToLink = [ "/libexec" ]; # links /libexec from derivations to /run/current-system/sw
+  services.xserver = {
+    enable = true;
+
+    desktopManager = {
+      xterm.enable = false;
+    };
+
+    displayManager = {
+      defaultSession = "none+i3";
+    };
+
+    windowManager.i3 = {
+      enable = true;
+      extraPackages = with pkgs; [
+        dmenu #application launcher most people use
+        i3status # gives you the default i3 status bar
+        i3lock #default i3 screen locker
+        i3blocks #if you are planning on using i3blocks over i3status
       ];
+    };
+  };
 
+  boot.initrd = {
+    luks.devices."root" = {
+      device = "/dev/disk/by-uuid/f35ee9fc-4d99-47de-936f-8c53e2b78b4a";
+      preLVM = true;
+      # keyFile = "/keyfile0.bin";
+      allowDiscards = true;
+    };
 
-      services.pipewire = {
-        enable = true;
-        alsa.enable = true;
-        pulse.enable = true;
-      };
+    luks.devices."data" = {
+      device = "/dev/disk/by-uuid/88001461-3665-4bdd-bf20-c0ca0a24abf9";
 
-      services.ddccontrol.enable = true;
+    };
+  };
 
-      virtualisation.libvirtd.enable = true;
-      virtualisation.libvirtd.qemu.ovmf.enable = true;
-      virtualisation.libvirtd.qemu.verbatimConfig = ''
-        nvram = [
-          "${pkgs.OVMF}/FV/OVMF.fd:${pkgs.OVMF}/FV/OVMF_VARS.fd"
-        ]
-        namespaces = []
-        user = "+1000"
-      '';
+  boot.loader = {
+    efi = {
+      efiSysMountPoint = "/boot/EFI";
+      canTouchEfiVariables = true;
+    };
+    systemd-boot = {
+      enable = true;
+      configurationLimit = 20;
+    };
+  };
 
+  networking.networkmanager.enable = true;
 
-      services.xserver.videoDrivers = [ "amdgpu" ];
+  # TODO: save passwords using agenix
+  users.mutableUsers = false;
+  users.users.root.initialPassword = "arstarst";
 
-      hardware.enableRedistributableFirmware = true;
-      hardware.opengl.enable = true;
-      hardware.opengl.driSupport = true;
+  fileSystems."/" =
+    {
+      device = "none";
+      fsType = "tmpfs";
+      options = [ "defaults" "size=2G" "mode=755" ];
+    };
 
-      # programs.sway.enable = true;
+  fileSystems."/nix" =
+    {
+      device = "/dev/vg/root";
+      fsType = "ext4";
+    };
 
-      networking.useDHCP = lib.mkDefault true;
+  fileSystems."/media/Steiner" =
+    {
+      device = "/dev/hdd-vg/data";
+      fsType = "btrfs";
+      options = [ "compress=zlib:6" ];
+    };
 
-      # hardware.cpu.amd.updateMicrocode = config.hardware.enableRedistributableFirmware;
+  fileSystems."/etc/nixos" =
+    {
+      device = "/nix/persist/etc/nixos";
+      fsType = "none";
+      options = [ "bind" ];
+    };
 
-      # high-resolution display
-      hardware.video.hidpi.enable = true;
-      environment.pathsToLink = [ "/libexec" ]; # links /libexec from derivations to /run/current-system/sw
-      services.xserver = {
-        enable = true;
+  fileSystems."/var/log" =
+    {
+      device = "/nix/persist/var/log";
+      fsType = "none";
+      options = [ "bind" ];
+    };
 
-        desktopManager = {
-          xterm.enable = false;
-        };
-
-        displayManager = {
-          defaultSession = "none+i3";
-        };
-
-        windowManager.i3 = {
-          enable = true;
-          extraPackages = with pkgs; [
-            dmenu #application launcher most people use
-            i3status # gives you the default i3 status bar
-            i3lock #default i3 screen locker
-            i3blocks #if you are planning on using i3blocks over i3status
-          ];
-        };
-      };
-
-      boot.initrd = {
-        luks.devices."root" = {
-          device = "/dev/disk/by-uuid/f35ee9fc-4d99-47de-936f-8c53e2b78b4a";
-          preLVM = true;
-          # keyFile = "/keyfile0.bin";
-          allowDiscards = true;
-        };
-
-        luks.devices."data" = {
-          device = "/dev/disk/by-uuid/88001461-3665-4bdd-bf20-c0ca0a24abf9";
-
-        };
-      };
-
-      boot.loader = {
-        efi = {
-          efiSysMountPoint = "/boot/EFI";
-          canTouchEfiVariables = true;
-        };
-        systemd-boot = {
-          enable = true;
-          configurationLimit = 20;
-        };
-      };
-
-      networking.networkmanager.enable = true;
-
-      # TODO: save passwords using agenix
-      users.mutableUsers = false;
-      users.users.root.initialPassword = "arstarst";
-
-      fileSystems."/" =
-        {
-          device = "none";
-          fsType = "tmpfs";
-          options = [ "defaults" "size=2G" "mode=755" ];
-        };
-
-      fileSystems."/nix" =
-        {
-          device = "/dev/vg/root";
-          fsType = "ext4";
-        };
-
-      fileSystems."/media/Steiner" =
-        {
-          device = "/dev/hdd-vg/data";
-          fsType = "btrfs";
-          options = [ "compress=zlib:6" ];
-        };
-
-      fileSystems."/etc/nixos" =
-        {
-          device = "/nix/persist/etc/nixos";
-          fsType = "none";
-          options = [ "bind" ];
-        };
-
-      fileSystems."/var/log" =
-        {
-          device = "/nix/persist/var/log";
-          fsType = "none";
-          options = [ "bind" ];
-        };
-
-      fileSystems."/boot/EFI" =
-        {
-          device = "/dev/disk/by-uuid/052E-D88B";
-          fsType = "vfat";
-        };
-      system.stateVersion = "22.05";
-    }
+  fileSystems."/boot/EFI" =
+    {
+      device = "/dev/disk/by-uuid/052E-D88B";
+      fsType = "vfat";
+    };
+  system.stateVersion = "22.05";
+}
