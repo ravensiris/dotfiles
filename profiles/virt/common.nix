5e92f42c967ff69578a222bfd27f8377563623dd
Maksymilian Jodlowski <maksymilian.jodlowski@gmail.com>
2022-08-14 22:53:56 +0000

    add swtpm
    fix swtpm
    
    fix swtpm
    
    fix swtpm

--- profiles/virt/common.nix
+++ profiles/virt/common.nix
@@ -6,15 +6,17 @@
     qemu
     OVMF
     pciutils
+    swtpm
   ];
 
   virtualisation.libvirtd.enable = true;
+  virtualisation.libvirtd.qemu.swtpm.enable = true;
   virtualisation.libvirtd.qemu.ovmf.enable = true;
-  virtualisation.libvirtd.qemu.verbatimConfig = ''
-    nvram = [
-      "${pkgs.OVMF}/FV/OVMF.fd:${pkgs.OVMF}/FV/OVMF_VARS.fd"
-    ]
-    namespaces = []
-    user = "+1000"
-  '';
+  # virtualisation.libvirtd.qemu.verbatimConfig = ''
+  #   nvram = [
+  #     "${pkgs.OVMF}/FV/OVMF.fd:${pkgs.OVMF}/FV/OVMF_VARS.fd"
+  #   ]
+  #   namespaces = []
+  #   user = "+1000"
+  # '';
 }
