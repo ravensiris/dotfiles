be64993b539e7741c75d68c7d2e2519d7d2f09c1
Maksymilian Jodlowski <maksymilian.jodlowski@gmail.com>
2022-08-14 19:41:51 +0000

    Disable rnix-lsp

--- users/profiles/doom-emacs/doom.d/config.el
+++ users/profiles/doom-emacs/doom.d/config.el
@@ -2,9 +2,9 @@
 (setq doom-font (font-spec :family "VictorMono Nerd Font" :size 18))
 
 ;; Setup lsp for nix
-(after! lsp-clients
-        (add-to-list 'lsp-language-id-configuration '(nix-mode . "nix"))
-        (lsp-register-client
-                (make-lsp-client :new-connection (lsp-stdio-connection '("rnix-lsp"))
-                                :major-modes '(nix-mode)
-                                :server-id 'nix)))
+;(after! lsp-clients
+;       (add-to-list 'lsp-language-id-configuration '(nix-mode . "nix"))
+;       (lsp-register-client
+;               (make-lsp-client :new-connection (lsp-stdio-connection '("rnix-lsp"))
+;                               :major-modes '(nix-mode)
+;                               :server-id 'nix)))
