460642545b53ee372a1ee20f1f602b62d9ba1283
Maksymilian Jodlowski <maksymilian.jodlowski@gmail.com>
2022-08-14 23:00:34 +0000

    virt fixes

--- profiles/virt/common.nix
+++ profiles/virt/common.nix
@@ -12,6 +12,7 @@
   virtualisation.libvirtd.enable = true;
   virtualisation.libvirtd.qemu.swtpm.enable = true;
   virtualisation.libvirtd.qemu.ovmf.enable = true;
+  environment.sessionVariables.LIBVIRT_DEFAULT_URI = [ "qemu:///system" ];
   # virtualisation.libvirtd.qemu.verbatimConfig = ''
   #   nvram = [
   #     "${pkgs.OVMF}/FV/OVMF.fd:${pkgs.OVMF}/FV/OVMF_VARS.fd"
