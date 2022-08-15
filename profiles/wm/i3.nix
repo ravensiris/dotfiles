d089ac730f4f1f34c01a05c54195056b6696471f
Maksymilian Jodłowski <maksymilian.jodlowski@gmail.com>
2022-08-15 13:06:25 +0000

    Disable accel in xserver

--- profiles/wm/i3.nix
+++ profiles/wm/i3.nix
@@ -4,7 +4,7 @@
   environment.pathsToLink = [ "/libexec" ]; # links /libexec from derivations to /run/current-system/sw
   services.xserver = {
     enable = true;
-
+    libinput.mouse.accelSpeed = "0.0";
     desktopManager = {
       xterm.enable = false;
     };
