84b22371e3647dd905679afc2f241e2763803803
Maksymilian Jodłowski <maksymilian.jodlowski@gmail.com>
2022-08-15 12:31:05 +0000

    Fix virtualization

--- profiles/virt/common.nix
+++ profiles/virt/common.nix
@@ -13,8 +13,8 @@
   virtualisation.libvirtd.enable = true;
   virtualisation.libvirtd.qemu.swtpm.enable = true;
   virtualisation.libvirtd.qemu.ovmf.enable = true;
-  virtualisation.libvirtd.qemu.ovmf.package = pkgs.OVMFFull;
-  virtualisation.libvirtd.qemu.runAsRoot = false;
+  # virtualisation.libvirtd.qemu.ovmf.package = pkgs.OVMFFull;
+  # virtualisation.libvirtd.qemu.runAsRoot = false;
   environment.sessionVariables.LIBVIRT_DEFAULT_URI = [ "qemu:///system" ];
   # virtualisation.libvirtd.qemu.verbatimConfig = ''
   #   nvram = [
