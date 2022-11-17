1815276b4850afb24919c3ca0b3b267ef175e279
Maksymilian Jodłowski <maksymilian.jodlowski@gmail.com>
2022-11-18 00:37:00 +0100

    Upate flake.lock

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -2,6 +2,5 @@ final: prev: rec{
   # keep sources this first
   sources = prev.callPackage (import ./_sources/generated.nix) { };
   anime4k = final.callPackage (import ./anime4k) { };
-  elixir_1_14_elixir_ls_0_12_0 = final.callPackage (import ./elixir_1_14_elixir_ls_0_12_0) { };
   # then, call packages with `final.callPackage`
 }
