dca5123e3c76586b4cc0c54caec5fd91ca596564
Maksymilian Jodłowski <maksymilian.jodlowski@gmail.com>
2022-11-17 20:56:04 +0100

    Fix elixir-ls not detected :doom:

--- users/profiles/doom-emacs/doom.d/config.el
+++ users/profiles/doom-emacs/doom.d/config.el
@@ -2,11 +2,24 @@
 (setq doom-font (font-spec :family "VictorMono Nerd Font" :size 18))
 
 ;; Setup lsp for nix
-;(after! lsp-clients
-;       (add-to-list 'lsp-language-id-configuration '(nix-mode . "nix"))
-;       (lsp-register-client
-;               (make-lsp-client :new-connection (lsp-stdio-connection '("rnix-lsp"))
-;                               :major-modes '(nix-mode)
-;                               :server-id 'nix)))
+                                        ;(after! lsp-clients
+                                        ;       (add-to-list 'lsp-language-id-configuration '(nix-mode . "nix"))
+                                        ;       (lsp-register-client
+                                        ;               (make-lsp-client :new-connection (lsp-stdio-connection '("rnix-lsp"))
+                                        ;                               :major-modes '(nix-mode)
+                                        ;                               :server-id 'nix)))
 
 (defun doom-modeline-set-vcs-modeline () 1)
+
+(defun lsp-put-elixir-ls-from-executable-find ()
+   (setq lsp-elixir-local-server-command (executable-find "elixir-ls")))
+
+(add-hook 'elixir-mode-hook #'lsp-put-elixir-ls-from-executable-find)
+
+(defun elixir-heex-format-on-save()
+  (interactive)
+  (when (and (eq major-mode 'web-mode)
+             (string-match-p "html.heex$" (buffer-file-name)))
+    (elixir-format)))
+
+(add-hook 'before-save-hook #'elixir-heex-format-on-save)
