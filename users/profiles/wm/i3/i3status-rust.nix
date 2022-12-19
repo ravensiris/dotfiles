9041d2e6716633d6df994a4984dd71a6620bf4a5
Maksymilian Jodłowski <maksymilian.jodlowski@gmail.com>
2022-12-19 11:12:27 +0100

    i3-status-rust blocks

--- users/profiles/wm/i3/i3status-rust.nix
+++ users/profiles/wm/i3/i3status-rust.nix
@@ -5,13 +5,88 @@
     enable = true;
     bars = {
       default = {
-        blocks = [{
-          block = "time";
-          interval = 15;
-          format = "%a %d/%m %R";
-        }];
-        icons = "awesome";
-        theme = "solarized-dark";
+        blocks =
+          [
+            {
+              block = "battery";
+              hide_missing = true;
+            }
+            {
+              block = "keyboard_layout";
+              driver = "setxkbmap";
+              on_click = ''setxkbmap $(setxkbmap -print | awk -F"+" '/xkb_symbols/ {if($2 == "us") print "pl"; else print "us";}')'';
+              format = "⌨ {layout}";
+              interval = 1;
+            }
+            {
+              block = "disk_space";
+              format = "{icon} <b>{used}</b>/{total}";
+              info_type = "available";
+              interval = 60;
+              path = "/nix/persist";
+              unit = "GB";
+              alert = 10.0;
+              warning = 20.0;
+            }
+            {
+              block = "xrandr";
+              icons = true;
+              resolution = false;
+            }
+            {
+              block = "sound";
+              format = "{volume}";
+              step_width = 1;
+              on_click = "${pkgs.alsa-utils}/bin/amixer set Master toggle";
+              headphones_indicator = true;
+            }
+            {
+              block = "sound";
+              format = "{volume}";
+              step_width = 1;
+              device_kind = "source";
+              on_click = "${pkgs.alsa-utils}/bin/amixer set Capture toggle";
+            }
+            {
+              block = "memory";
+              format_mem = "<b>{mem_used}</b> /{mem_total}";
+              display_type = "memory";
+              icons = true;
+              clickable = false;
+              interval = 5;
+              warning_mem = 80;
+              warning_swap = 80;
+              critical_mem = 95;
+              critical_swap = 95;
+              theme_overrides = {
+                idle_bg = "#2aa198";
+                idle_fg = "#002b36";
+              };
+            }
+            {
+              block = "cpu";
+              interval = 1;
+              format = "{utilization} {frequency}";
+              theme_overrides = {
+                idle_bg = "#268bd2";
+                idle_fg = "#002b36";
+              };
+            }
+            {
+              block = "time";
+              interval = 15;
+              format = "%a %d/%m <span font_weight='900' font_family='Nova Mono'>%R</span>";
+              theme_overrides = {
+                idle_bg = "#6c71c4";
+                idle_fg = "#002b36";
+              };
+            }
+          ];
+        settings = {
+          icons = "material-nf";
+          theme = "solarized-dark";
+          icons_format = " {icon} ";
+        };
       };
     };
   };
