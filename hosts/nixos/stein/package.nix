a7b46d85fc679697447c8e94d898168278246f4c
Maksymilian Jodłowski <maksymilian.jodlowski@gmail.com>
2022-12-19 11:12:54 +0100

    Add wine & steam

--- hosts/nixos/stein/package.nix
+++ hosts/nixos/stein/package.nix
@@ -1,10 +1,28 @@
 { config, lib, pkgs, ... }:
 
 {
+  nixpkgs.config.allowUnfreePredicate = pkg: builtins.elem (lib.getName pkg) [
+    "steam"
+    "steam-original"
+    "steam-runtime"
+  ];
+
   environment.systemPackages = with pkgs; [
     brightnessctl
     neovim
     tenacity
     gnupg
+    virtualbox
+    wineWowPackages.staging
+    winetricks
+    lutris
   ];
+  virtualisation.virtualbox.host.enable = true;
+  users.extraGroups.vboxusers.members = [ "q" ];
+
+  programs.steam = {
+    enable = true;
+    # remotePlay.openFirewall = true; # Open ports in the firewall for Steam Remote Play
+    # dedicatedServer.openFirewall = true; # Open ports in the firewall for Source Dedicated Server
+  };
 }
