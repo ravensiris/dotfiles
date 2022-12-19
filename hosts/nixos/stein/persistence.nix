4d06586abe6f27d649763fe60bf04630cba2fec6
Maksymilian Jodłowski <maksymilian.jodlowski@gmail.com>
2022-12-19 11:12:12 +0100

    Additional persistence directories

--- hosts/nixos/stein/persistence.nix
+++ hosts/nixos/stein/persistence.nix
@@ -4,6 +4,9 @@
   environment.persistence."/nix/persist" = {
     directories = [
       "/var/lib/docker"
+      "/etc/NetworkManager/system-connections/"
     ];
+    users.q.directories = [ "Projects" ".wine" ".local/share/lutris" "Games" ".config/lutris" "Pictures" ];
   };
+
 }
