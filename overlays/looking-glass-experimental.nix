c9856efbfa8626c3cb32d2bb60c09a782af7c162
Maksymilian Jodłowski <maksymilian.jodlowski@gmail.com>
2023-01-08 01:13:54 +0100

    Delete looking-glass experimental overlay

--- overlays/looking-glass-experimental.nix
+++
@@ -1,9 +0,0 @@
-final: prev: {
-  looking-glass-client = prev.looking-glass-client.overrideAttrs (o: rec {
-    version = "B6";
-    src = prev.fetchFromGitHub {
-      rev = version;
-      sha256 = "";
-    };
-  });
-}
