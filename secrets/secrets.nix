9c64bc44acad0850f05768557e56d3d88a654d9c
Maksymilian Jodłowski <maksymilian.jodlowski@gmail.com>
2023-01-18 14:43:24 +0100

    [global] rekey

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -9,6 +9,6 @@ in
 {
   "users/root.age".publicKeys = allKeys;
   "users/q.age".publicKeys = allKeys;
-  "tokens/google_api_token.age".publicKeys = [q-gate q-stein];
-  "tokens/lastfm_token.age".publicKeys = [q-gate q-stein];
+  "tokens/google_api_token.age".publicKeys = allKeys;
+  "tokens/lastfm_token.age".publicKeys = allKeys;
 }
