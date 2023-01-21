b8eb2e904648e46338e471496ce28ab20099e32f
Maksymilian Jodłowski <maksymilian.jodlowski@gmail.com>
2023-01-21 03:14:55 +0100

    [stein] Add battery for i3-status on stein

--- hosts/nixos/stein/device.nix
+++ hosts/nixos/stein/device.nix
@@ -2,4 +2,11 @@
 
 {
   services.xserver.libinput.enable = true;
+
+  # extend i3status-rust with battery
+  home-manager.users.q.programs.i3status-rust.bars.default.blocks = [
+    {
+      block = "battery";
+    }
+  ];
 }
