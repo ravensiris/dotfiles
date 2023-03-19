288cc7c9a2adf74d21f35b1a450424f9d459fd84
Maksymilian Jodłowski <maksymilian.jodlowski@gmail.com>
2023-03-19 15:15:29 +0100

    Swaylock fixes

--- profiles/wm/sway.nix
+++ profiles/wm/sway.nix
@@ -185,8 +185,24 @@
   '';
 
   security.polkit.enable = true;
+  security.pam.services.swaylock.text = ''
+    # Account management.
+    account required pam_unix.so
+
+    # Authentication management.
+    auth sufficient pam_unix.so   likeauth try_first_pass
+    auth required pam_deny.so
+
+    # Password management.
+    password sufficient pam_unix.so nullok sha512
+
+    # Session management.
+    session required pam_env.so conffile=/etc/pam/environment readenv=0
+    session required pam_unix.so
+  '';
 
   fonts.fonts = with pkgs; [
     koruri
   ];
+  environment.systemPackages = with pkgs; [ swaylock ];
 }
