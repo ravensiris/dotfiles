da474b4ec60489c2bc78c4deb5e2723a60139070
Maksymilian Jodłowski <maksymilian.jodlowski@gmail.com>
2023-04-01 02:25:51 +0200

    Fix pipewire not switching rates

--- profiles/audio/pipewire.nix
+++ profiles/audio/pipewire.nix
@@ -5,6 +5,13 @@
     enable = true;
     alsa.enable = true;
     pulse.enable = true;
+
+    config.pipewire = {
+      "context.properties" = {
+        "avoid-resampling" = true;
+        "default.clock.allowed-rates" = [ 44100 48000 96000 192000 ];
+      };
+    };
   };
 
   environment.systemPackages = with pkgs; [
