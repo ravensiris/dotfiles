0ccb9284efc0ff0e538b399abb40a8b3ed0168ef
Maksymilian Jodłowski <maksymilian.jodlowski@gmail.com>
2023-05-17 00:35:40 +0200

    Use forked digga

--- flake.nix
+++ flake.nix
@@ -18,7 +18,7 @@
       # failures on Linux systems.
       nixpkgs-darwin-stable.url = "github:NixOS/nixpkgs/nixpkgs-22.11-darwin";
 
-      digga.url = "github:divnix/digga";
+      digga.url = "github:ravensiris/digga";
       digga.inputs.nixpkgs.follows = "nixos";
       digga.inputs.nixlib.follows = "nixos";
       digga.inputs.home-manager.follows = "home";
