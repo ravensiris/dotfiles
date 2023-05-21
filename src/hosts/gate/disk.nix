5f314aa1909a39efefb60647fbd20fface48f7da
Maksymilian Jodłowski <maksymilian.jodlowski@gmail.com>
2023-05-21 02:30:38 +0200

    Fix

--- src/hosts/gate/disk.nix
+++ src/hosts/gate/disk.nix
@@ -1,7 +1,4 @@
-{
-  dropOverrides,
-  ...
-}: {
+{dropOverrides, root, ...}: {
   disko.devices =
     dropOverrides (root.partitioning.encryptedLVMWithTmpfsRoot.override
       {disks = ["/dev/vda"];});
