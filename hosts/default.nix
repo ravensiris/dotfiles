99e1f76b5e2af9f123efcbbd3c9d644fb1e8f027
Maksymilian Jodłowski <maksymilian.jodlowski@gmail.com>
2023-05-27 08:29:12 +0200

    Rm unused

--- hosts/default.nix
+++ hosts/default.nix
@@ -1,19 +1,13 @@
 {
   lib,
-  inputs,
-  nixpkgs,
-  home-manager,
   disko,
-  user,
   ...
 }: {
   gate = lib.nixosSystem {
     system = "x86_64-linux";
     specialArgs = {
-      inherit user;
     };
     modules = [
-      ./configuration.nix
       disko.nixosModules.disko
       ./gate
     ];
