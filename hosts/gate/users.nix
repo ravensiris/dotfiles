26ef044907d38e0be083cebc2ed74d92746d8b58
Maksymilian Jodłowski <maksymilian.jodlowski@gmail.com>
2023-05-28 11:12:20 +0200

    Fix

--- hosts/gate/users.nix
+++ hosts/gate/users.nix
@@ -5,11 +5,13 @@
 
   # these are host specific
   # for more general ones set them using home-manager
-  environment.persistence."/nix/persist".users.q.directories = {...}: [
-    "Projects"
-    "Documents"
-    "Sync"
-    ".config/syncthing"
-    "Music"
-  ];
+  environment.persistence."/nix/persist" = {
+    users.q.directories = [
+      "Projects"
+      "Documents"
+      "Sync"
+      ".config/syncthing"
+      "Music"
+    ];
+  };
 }
