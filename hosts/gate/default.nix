bed506854ce2fa4250f0e22e6296ce6ca03d15d3
Maksymilian Jodłowski <maksymilian.jodlowski@gmail.com>
2023-05-28 11:28:35 +0200

    Setup linux console font

--- hosts/gate/default.nix
+++ hosts/gate/default.nix
@@ -1,4 +1,8 @@
-{lib, pkgs, ...}: {
+{
+  lib,
+  pkgs,
+  ...
+}: {
   imports = [
     (import ./disk.nix {
       inherit lib;
@@ -9,12 +13,42 @@
     ./users.nix
   ];
 
-  i18n.defaultLocale = "en_US.UTF-8";
-  console = {
-    earlySetup = true;
-    font = "${pkgs.terminus_font}/share/consolefonts/ter-132n.psf.gz";
-    packages = with pkgs; [terminus_font];
-    keyMap = "us";
+  fonts = {
+    enableDefaultFonts = true;
+    fonts = with pkgs; [
+      #font-awesome_4
+      corefonts # Microsoft free fonts
+      dejavu_fonts
+      fira
+      fira-mono
+      line-awesome
+      google-fonts
+      inconsolata # monospaced
+      libertine
+      mononoki
+      nerdfonts
+      open-dyslexic
+      overpass
+      oxygenfonts
+      powerline-fonts
+      source-code-pro
+      source-sans-pro
+      source-serif-pro
+      ttf_bitstream_vera
+      ubuntu_font_family # Ubuntu fonts
+      unifont # some international languages
+    ];
+    fontconfig = {
+      antialias = true;
+      cache32Bit = true;
+      hinting.enable = true;
+      hinting.autohint = true;
+      defaultFonts = {
+        monospace = ["Source Code Pro"];
+        sansSerif = ["Source Sans Pro"];
+        serif = ["Source Serif Pro"];
+      };
+    };
   };
 
   nix.settings.experimental-features = ["nix-command" "flakes"];
