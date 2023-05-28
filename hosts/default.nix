b0950617bcd253f7cfd7f04040f544a133d4c8ca
Maksymilian Jodłowski <maksymilian.jodlowski@gmail.com>
2023-05-28 10:25:11 +0200

    Fix

--- hosts/default.nix
+++ hosts/default.nix
@@ -2,12 +2,15 @@
   lib,
   disko,
   home-manager,
+  impermanence,
   ...
 }: {
   gate = lib.nixosSystem {
     system = "x86_64-linux";
     modules = [
-      home-manager.nixosModules.home-manager
+      home-manager.nixosModules.home-manager {
+        inherit impermanence;
+      }
       disko.nixosModules.disko
       ./gate
     ];
