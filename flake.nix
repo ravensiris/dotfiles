a7864f3eff5d23276e62d2b59142e83712d29363
Maksymilian Jodłowski <maksymilian.jodlowski@gmail.com>
2023-05-28 10:26:57 +0200

    Fix

--- flake.nix
+++ flake.nix
@@ -4,7 +4,10 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/22.11";
     home-manager.url = "github:nix-community/home-manager/release-22.11";
-    impermanence.url = "github:nix-community/impermanence";
+    impermanence = {
+      url = "github:nix-community/impermanence";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
     disko.url = "github:nix-community/disko";
     disko.inputs.nixpkgs.follows = "nixpkgs";
   };
