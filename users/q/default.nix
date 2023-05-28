0cf6b4988794a60e252eea013677bf7d28ddd8ac
Maksymilian Jodłowski <maksymilian.jodlowski@gmail.com>
2023-05-28 10:13:06 +0200

    HM setup fish shell

--- users/q/default.nix
+++ users/q/default.nix
@@ -1,14 +1,66 @@
-{...}: {
+{ pkgs, ... }: {
   users.mutableUsers = false;
   users.users.q = {
     # passwordFile = "/run/agenix/qPassword";
     password = "arstarst";
     isNormalUser = true;
     extraGroups = ["wheel" "libvirtd" "docker" "adbusers" "input"];
+    shell = pkgs.fish;
   };
 
   home-manager.users.q = {pkgs, ...}: {
-    home.packages = [pkgs.neovim];
+    home.packages = [
+      pkgs.neovim
+    ];
+    home.persistence."/nix/persist/home/q" = {
+      files = [
+        ".ssh/id_rsa"
+        ".ssh/id_rsa.pub"
+        ".local/share/nix/trusted-settings.json"
+        ".local/share/beets/musiclibrary.db"
+        ".config/fish/completions/beet.fish"
+        ".local/share/fish/fish_history"
+      ];
+      directories = [
+        ".gnupg"
+        ".password-store"
+        ".mozilla"
+        ".config/BraveSoftware/Brave-Browser"
+        ".config/musikcube"
+        ".config/Sonixd"
+        ".local/share/yuzu"
+        ".config/easyeffects"
+      ];
+      allowOther = true;
+    };
+
+    home.programs = {
+      fish = {
+        enable = true;
+        interactiveShellInit = ''
+          function fish_greeting
+            if [ (tput cols) -ge 40 ]; and [ (tput lines) -ge 18 ]; ${pkgs.nitch}/bin/nitch; end
+          end
+        '';
+        plugins = with pkgs.fishPlugins; [
+          {
+            name = "pure";
+            src = pure.src;
+          }
+        ];
+        shellAliases = {
+          "du" = "${pkgs.du-dust}/bin/dust";
+          "ls" = "${pkgs.exa}/bin/exa --icons";
+          "la" = "${pkgs.exa}/bin/exa --icons -la";
+          "cat" = "${pkgs.bat}/bin/bat";
+          "df" = "${pkgs.duf}/bin/duf";
+          "ps" = "${pkgs.procs}/bin/procs";
+          "curl" = "${pkgs.curlie}/bin/curlie";
+          "dig" = "${pkgs.dogdns}/bin/dog";
+          "cp" = "${pkgs.xcp}/bin/xcp";
+        };
+      };
+    };
     home.stateVersion = "22.11";
   };
 }
