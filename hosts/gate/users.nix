f7bb26d76cbb364e61a8d9daad2251f9ad64cb24
Maksymilian Jodłowski <maksymilian.jodlowski@gmail.com>
2023-05-28 11:03:52 +0200

    Add docstr

--- hosts/gate/users.nix
+++ hosts/gate/users.nix
@@ -1,11 +1,13 @@
 {...}: {
   imports = [../../users/q/default.nix];
+
+  # these are host specific
+  # for more general ones set them using home-manager
   environment.persistence."/nix/persist".users.q.directories = [
-      "Projects"
-      "Documents"
-      "Sync"
-      ".config/syncthing"
-      "Music"
-    ];
-    
+    "Projects"
+    "Documents"
+    "Sync"
+    ".config/syncthing"
+    "Music"
+  ];
 }
