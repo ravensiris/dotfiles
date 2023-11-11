e4670f017c40d0931d327dffdeb91dc25c9143d7
Maksymilian Jodłowski <maksymilian.jodlowski@gmail.com>
2023-11-11 19:48:37 +0100

    :wrench: Setup khinsider

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -1,2 +1,25 @@
-{}: {
+{
+  lib,
+  pkgs,
+  stdenv,
+  fetchFromGitHub,
+  fetchurl,
+  dockerTools,
+  fetchgit,
+  ...
+}: let
+  callPackage' = path: overrides: let
+    f = import path;
+  in
+    f ((builtins.intersectAttrs (builtins.functionArgs f) pkgs) // overrides);
+
+  sources = callPackage' _sources/generated.nix {};
+  callPackage = path: overrides:
+    callPackage' path (overrides // {sources = sources;});
+in {
+  nixpkgs.overlays = [
+    (final: prev: {
+      khinsider = callPackage ./khinsider {};
+    })
+  ];
 }
