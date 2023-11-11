c23da98cd6e9ae197b57435b325a0de8ba1fa38f
Maksymilian Jodłowski <maksymilian.jodlowski@gmail.com>
2023-11-11 19:31:50 +0100

    :wrench: Setup nvfetcher

--- devenv.nix
+++ devenv.nix
@@ -1,5 +1,7 @@
-{ pkgs, ... }:
-
-{
-  packages = [ pkgs.git  (pkgs.callPackage "${builtins.fetchTarball "https://github.com/ryantm/agenix/archive/main.tar.gz"}/pkgs/agenix.nix" {})];
+{pkgs, ...}: {
+  packages = [
+    pkgs.git
+    (pkgs.callPackage "${builtins.fetchTarball "https://github.com/ryantm/agenix/archive/main.tar.gz"}/pkgs/agenix.nix" {})
+    pkgs.nvfetcher
+  ];
 }
