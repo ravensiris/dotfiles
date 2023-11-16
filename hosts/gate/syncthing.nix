68c738f51bd2f812e0ae0eeb17cf7e71ea232466
Maksymilian Jodłowski <maksymilian.jodlowski@gmail.com>
2023-11-16 20:46:26 +0100

    :fire: Add firewall exception for syncthing

--- hosts/gate/syncthing.nix
+++ hosts/gate/syncthing.nix
@@ -5,4 +5,9 @@
     dataDir = "/home/q/Documents";
     configDir = "/home/q/Documents/.config/syncthing";
   };
+
+  networking.firewall = {
+    allowedTCPPorts = [22000];
+    allowedUDPPorts = [22000 21027];
+  };
 }
