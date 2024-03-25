6557722677f5e2e9943b2fc66189deb0bdb05044
Maksymilian Jodłowski <maksymilian.jodlowski@gmail.com>
2024-03-26 00:44:21 +0100

    🔭 Open telescope on VimEnter

--- users/q/neovim/neovim/init.lua
+++ users/q/neovim/neovim/init.lua
@@ -8,4 +8,12 @@ require 'keymaps'
 require 'lazy-bootstrap'
 require 'lazy-plugins'
 
+vim.api.nvim_create_autocmd('VimEnter', {
+  callback = function()
+    if vim.fn.argv(0) == '' then
+      require('telescope.builtin').find_files()
+    end
+  end,
+})
+
 -- vim: ts=2 sts=2 sw=2 et
