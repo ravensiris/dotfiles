7e9ae034ee8b959ff51084ce16186ad4b703cb5d
Maksymilian Jodłowski <maksymilian.jodlowski@gmail.com>
2024-03-26 04:21:05 +0100

    🧘 Add impermanence option to firefox

--- users/q/firefox.nix
+++ users/q/firefox.nix
@@ -1,47 +1,63 @@
-{pkgs, ...}: {
-  programs.firefox = {
-    enable = true;
-    profiles.default = {
-      extensions = with pkgs.nur.repos.rycee.firefox-addons; [
-        ublock-origin
-      ];
-      id = 0;
-      name = "Default";
-    };
-    profiles.personal = {
-      extensions = with pkgs.nur.repos.rycee.firefox-addons; [
-        ublock-origin
-      ];
-      id = 1;
-      name = "Personal";
+{
+  pkgs,
+  lib,
+  config,
+  ...
+}: {
+  options.firefox.impermanence = lib.mkOption {
+    type = lib.types.bool;
+    default = true;
+  };
+
+  config = {
+    programs.firefox = {
+      enable = true;
+      profiles.default = {
+        extensions = with pkgs.nur.repos.rycee.firefox-addons; [
+          ublock-origin
+        ];
+        id = 0;
+        name = "Default";
+      };
+      profiles.personal = {
+        extensions = with pkgs.nur.repos.rycee.firefox-addons; [
+          ublock-origin
+        ];
+        id = 1;
+        name = "Personal";
+      };
+
+      profiles.work = {
+        extensions = with pkgs.nur.repos.rycee.firefox-addons; [
+          ublock-origin
+        ];
+        id = 2;
+        name = "Work";
+      };
     };
 
-    profiles.work = {
-      extensions = with pkgs.nur.repos.rycee.firefox-addons; [
-        ublock-origin
-      ];
-      id = 2;
-      name = "Work";
-    };
-  };
-  xdg = {
-    desktopEntries = {
-      firefox = {
-        name = "Firefox";
-        genericName = "Web Browser";
-        exec = "firefox %U";
-        terminal = false;
-        categories = ["Application" "Network" "WebBrowser"];
-        mimeType = ["text/html" "text/xml"];
-        actions = {
-          "Work-Profile" = {
-            exec = ''${pkgs.firefox}/bin/firefox -P "Work"'';
-            name = "Work Profile";
+    xdg = {
+      desktopEntries = {
+        firefox = {
+          name = "Firefox";
+          genericName = "Web Browser";
+          exec = "firefox %U";
+          terminal = false;
+          categories = ["Application" "Network" "WebBrowser"];
+          mimeType = ["text/html" "text/xml"];
+          actions = {
+            "Work-Profile" = {
+              exec = ''${pkgs.firefox}/bin/firefox -P "Work"'';
+              name = "Work Profile";
+            };
           };
         };
       };
     };
+    home =
+      {}
+      // (lib.optionalAttrs config.firefox.impermanence {
+        persistence."/nix/persist/home/q".directories = [".mozilla"];
+      });
   };
-
-  home.persistence."/nix/persist/home/q".directories = [".mozilla"];
 }
