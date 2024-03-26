49569bfff45c5579b4945864eb007e03f205aeff
Maksymilian Jodłowski <maksymilian.jodlowski@gmail.com>
2024-03-26 04:20:46 +0100

    🧘 Add impermanence option to fish

--- users/q/fish.nix
+++ users/q/fish.nix
@@ -1,35 +1,49 @@
-{pkgs, ...}: {
-  programs.fish = {
-    enable = true;
-    interactiveShellInit = ''
-       function fish_greeting
-      if [ (tput cols) -ge 40 ]; and [ (tput lines) -ge 18 ]; ${pkgs.nitch}/bin/nitch; end
-       end
-    '';
-    plugins = with pkgs.fishPlugins; [
-      {
-        name = "pure";
-        src = pure.src;
-      }
-    ];
-    shellAliases = {
-      "du" = "${pkgs.du-dust}/bin/dust";
-      "ls" = "${pkgs.unstable.eza}/bin/eza --icons";
-      "la" = "${pkgs.unstable.eza}/bin/eza --icons -la --extended --git";
-      "cat" = "${pkgs.bat}/bin/bat";
-      "df" = "${pkgs.duf}/bin/duf";
-      "ps" = "${pkgs.procs}/bin/procs";
-      "curl" = "${pkgs.curlie}/bin/curlie";
-      "dig" = "${pkgs.dogdns}/bin/dog";
-      "cp" = "${pkgs.xcp}/bin/xcp";
+{
+  pkgs,
+  lib,
+  config,
+  ...
+}: {
+  options.fish.impermanence = lib.mkOption {
+    type = lib.types.bool;
+    default = true;
+  };
+
+  config = {
+    programs.fish = {
+      enable = true;
+      interactiveShellInit = ''
+         function fish_greeting
+        if [ (tput cols) -ge 40 ]; and [ (tput lines) -ge 18 ]; ${pkgs.nitch}/bin/nitch; end
+         end
+      '';
+      plugins = with pkgs.fishPlugins; [
+        {
+          name = "pure";
+          src = pure.src;
+        }
+      ];
+      shellAliases = {
+        "du" = "${pkgs.du-dust}/bin/dust";
+        "ls" = "${pkgs.unstable.eza}/bin/eza --icons";
+        "la" = "${pkgs.unstable.eza}/bin/eza --icons -la --extended --git";
+        "cat" = "${pkgs.bat}/bin/bat";
+        "df" = "${pkgs.duf}/bin/duf";
+        "ps" = "${pkgs.procs}/bin/procs";
+        "curl" = "${pkgs.curlie}/bin/curlie";
+        "dig" = "${pkgs.dogdns}/bin/dog";
+        "cp" = "${pkgs.xcp}/bin/xcp";
+      };
     };
-  };
 
-  home.persistence."/nix/persist/home/q" = {
-    files = [
-      ".local/share/fish/fish_history"
-    ];
-  };
+    programs.kitty.settings.shell = "${pkgs.fish}/bin/fish";
 
-  programs.kitty.settings.shell = "${pkgs.fish}/bin/fish";
+    home =
+      {}
+      // (lib.optionalAttrs config.firefox.impermanence {
+        persistence."/nix/persist/home/q".directories = [
+          ".local/share/fish/fish_history"
+        ];
+      });
+  };
 }
