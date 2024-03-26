35a9d1b0e82016caccdd467bc9b30c9898a567ec
Maksymilian Jodłowski <maksymilian.jodlowski@gmail.com>
2024-03-26 03:34:45 +0100

    📜 Make neovim impermanence optional

--- users/q/neovim/default.nix
+++ users/q/neovim/default.nix
@@ -1,33 +1,47 @@
-{pkgs, ...}: {
-  xdg = {
-    enable = true;
-    configFile."nvim" = {
-      source = ./neovim;
-      recursive = true;
+{
+  lib,
+  pkgs,
+  config,
+  ...
+}: {
+  options.neovim.impermanence = lib.mkOption {
+    type = lib.types.bool;
+    default = true;
+  };
+
+  config = {
+    xdg = {
+      enable = true;
+      configFile."nvim" = {
+        source = ./neovim;
+        recursive = true;
+      };
     };
+    home =
+      {
+        packages = with pkgs; [
+          neovim
+          alejandra
+          black
+          ruff
+          isort
+          mypy
+          ripgrep
+          stylua
+          lua-language-server
+          gnumake
+          gcc
+          elixir-ls
+        ];
+        sessionVariables = {EDITOR = "nvim";};
+      }
+      // lib.optionalAttrs config.neovim.impermanence {
+        persistence."/nix/persist/home/q" = {
+          directories = [
+            ".local/share/nvim"
+            ".cache/nvim"
+          ];
+        };
+      };
   };
-
-  home.packages = with pkgs; [
-    neovim
-    alejandra
-    black
-    ruff
-    isort
-    mypy
-    ripgrep
-    stylua
-    lua-language-server
-    gnumake
-    gcc
-    elixir-ls
-  ];
-
-  home.persistence."/nix/persist/home/q" = {
-    directories = [
-      ".local/share/nvim"
-      ".cache/nvim"
-    ];
-  };
-
-  home.sessionVariables = { EDITOR = "nvim"; };
 }
