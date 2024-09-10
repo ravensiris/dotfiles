61453f0375c5bfae6fe9935c324bbec53742a1e3
Maksymilian Jodłowski <maksymilian.jodlowski@gmail.com>
2024-09-10 22:57:21 +0200

    🧹 Clean old artifacts

--- users/q/firefox.nix
+++ users/q/firefox.nix
@@ -4,60 +4,49 @@
   config,
   ...
 }: {
-  options.firefox.impermanence = lib.mkOption {
-    type = lib.types.bool;
-    default = true;
-  };
-
-  config = {
-    programs.firefox = {
-      enable = false;
-      profiles.default = {
-        extensions = with pkgs.nur.repos.rycee.firefox-addons; [
-          ublock-origin
-        ];
-        id = 0;
-        name = "Default";
-      };
-      profiles.personal = {
-        extensions = with pkgs.nur.repos.rycee.firefox-addons; [
-          ublock-origin
-        ];
-        id = 1;
-        name = "Personal";
-      };
-
-      profiles.work = {
-        extensions = with pkgs.nur.repos.rycee.firefox-addons; [
-          ublock-origin
-        ];
-        id = 2;
-        name = "Work";
-      };
+  programs.firefox = {
+    enable = false;
+    profiles.default = {
+      extensions = with pkgs.nur.repos.rycee.firefox-addons; [
+        ublock-origin
+      ];
+      id = 0;
+      name = "Default";
+    };
+    profiles.personal = {
+      extensions = with pkgs.nur.repos.rycee.firefox-addons; [
+        ublock-origin
+      ];
+      id = 1;
+      name = "Personal";
     };
 
-    xdg = {
-      desktopEntries = {
-        firefox = {
-          name = "Firefox";
-          genericName = "Web Browser";
-          exec = "firefox %U";
-          terminal = false;
-          categories = ["Application" "Network" "WebBrowser"];
-          mimeType = ["text/html" "text/xml"];
-          actions = {
-            "Work-Profile" = {
-              exec = ''${pkgs.firefox}/bin/firefox -P "Work"'';
-              name = "Work Profile";
-            };
+    profiles.work = {
+      extensions = with pkgs.nur.repos.rycee.firefox-addons; [
+        ublock-origin
+      ];
+      id = 2;
+      name = "Work";
+    };
+  };
+
+  xdg = {
+    desktopEntries = {
+      firefox = {
+        name = "Firefox";
+        genericName = "Web Browser";
+        exec = "firefox %U";
+        terminal = false;
+        categories = ["Application" "Network" "WebBrowser"];
+        mimeType = ["text/html" "text/xml"];
+        actions = {
+          "Work-Profile" = {
+            exec = ''${pkgs.firefox}/bin/firefox -P "Work"'';
+            name = "Work Profile";
           };
         };
       };
     };
-    home =
-      {}
-      // (lib.optionalAttrs config.firefox.impermanence {
-        persistence."/nix/persist/home/q".directories = [".mozilla"];
-      });
   };
+  home.persistence."/nix/persist/home/q".directories = [".mozilla"];
 }
--- users/q/neovim/default.nix
+++ users/q/neovim/default.nix
@@ -4,44 +4,34 @@
   config,
   ...
 }: {
-  options.neovim.impermanence = lib.mkOption {
-    type = lib.types.bool;
-    default = true;
-  };
-
-  config = {
-    xdg = {
-      enable = true;
-      configFile."nvim" = {
-        source = ./neovim;
-        recursive = true;
-      };
+  xdg = {
+    enable = true;
+    configFile."nvim" = {
+      source = ./neovim;
+      recursive = true;
     };
-    home =
-      {
-        packages = with pkgs; [
-          unstable.neovim
-          alejandra
-          black
-          ruff
-          isort
-          mypy
-          ripgrep
-          stylua
-          lua-language-server
-          gnumake
-          gcc
-          elixir-ls
-        ];
-        sessionVariables = {EDITOR = "nvim";};
-      }
-      // lib.optionalAttrs config.neovim.impermanence {
-        persistence."/nix/persist/home/q" = {
-          directories = [
-            ".local/share/nvim"
-            ".cache/nvim"
-          ];
-        };
-      };
+  };
+  home = {
+    packages = with pkgs; [
+      unstable.neovim
+      alejandra
+      black
+      ruff
+      isort
+      mypy
+      ripgrep
+      stylua
+      lua-language-server
+      gnumake
+      gcc
+      elixir-ls
+    ];
+    sessionVariables = {EDITOR = "nvim";};
+  };
+  home.persistence."/nix/persist/home/q" = {
+    directories = [
+      ".local/share/nvim"
+      ".cache/nvim"
+    ];
   };
 }
