61361586a3540c838233a74bb7b9c0d113eaed38
Maksymilian Jodłowski <maksymilian.jodlowski@gmail.com>
2024-10-02 17:26:28 +0200

    🐛 Fix attrsets not merging properly

--- users/q/sway.nix
+++ users/q/sway.nix
@@ -35,12 +35,16 @@
   '';
 
   mkMpvPaperService = monitorName: {
-    systemd.user.services."mpvpaper@${monitorName}" = {
+    systemd.user.services."mpvpaper-${monitorName}" = {
       Unit = {
         Description = "Animated wallpaper daemon for Sway %I output";
         PartOf = "sway-session.target";
         Requires = "sway-session.target";
         After = "sway-session.target";
+      };
+
+      Install = {
+        WantedBy = ["sway-session.target"];
       };
 
       Service = {
@@ -50,226 +54,228 @@
     };
   };
 in
-  (mkMpvPaperService "GIGA-BYTE")
-  // (mkMpvPaperService "LG")
-  // {
-    home.packages = with pkgs; [playerctl mpvpaper_set_for_output lock_command];
+  lib.mkMerge [
+    (mkMpvPaperService "GIGA-BYTE")
+    (mkMpvPaperService "LG")
+    {
+      home.packages = with pkgs; [playerctl mpvpaper_set_for_output lock_command];
 
-    home.pointerCursor = {
-      name = "Numix-Cursor";
-      package = pkgs.numix-cursor-theme;
-      gtk.enable = true;
-      x11.enable = true;
-    };
+      home.pointerCursor = {
+        name = "Numix-Cursor";
+        package = pkgs.numix-cursor-theme;
+        gtk.enable = true;
+        x11.enable = true;
+      };
 
-    services.swayidle = {
-      enable = true;
-      events = [
-        {
-          event = "before-sleep";
-          command = "${lock_command}/bin/lock_command";
-        }
-      ];
-    };
+      services.swayidle = {
+        enable = true;
+        events = [
+          {
+            event = "before-sleep";
+            command = "${lock_command}/bin/lock_command";
+          }
+        ];
+      };
 
-    services.kanshi = {
-      enable = true;
-      settings = [
-        {
-          profile.name = "desktop";
-          profile.outputs = [
-            {
-              criteria = gigabyte_4k_monitor;
-              position = "1409,720";
-            }
-            {
-              criteria = lg_dualup_monitor;
-              position = "5249,414";
-            }
-          ];
-        }
-      ];
-    };
+      services.kanshi = {
+        enable = true;
+        settings = [
+          {
+            profile.name = "desktop";
+            profile.outputs = [
+              {
+                criteria = gigabyte_4k_monitor;
+                position = "1409,720";
+              }
+              {
+                criteria = lg_dualup_monitor;
+                position = "5249,414";
+              }
+            ];
+          }
+        ];
+      };
 
-    programs.waybar = {
-      enable = true;
-      systemd.enable = true;
-      style = ./waybar.css;
-      settings = {
-        sideBar = {
-          layer = "top";
-          position = "top";
-          height = 16;
-          output = [
-            lg_dualup_monitor
-          ];
-          modules-left = ["sway/workspaces"];
+      programs.waybar = {
+        enable = true;
+        systemd.enable = true;
+        style = ./waybar.css;
+        settings = {
+          sideBar = {
+            layer = "top";
+            position = "top";
+            height = 16;
+            output = [
+              lg_dualup_monitor
+            ];
+            modules-left = ["sway/workspaces"];
 
-          "sway/workspaces" = {
-            disable-scroll = true;
-            all-outputs = false;
-          };
-        };
-        mainBar = {
-          layer = "top";
-          position = "top";
-          height = 16;
-          output = [
-            gigabyte_4k_monitor
-            "eDP-1"
-          ];
-          modules-left = ["sway/workspaces" "mpris"];
-          modules-center = ["clock"];
-          modules-right = ["battery" "memory" "cpu" "tray"];
-
-          battery = {
-            format = "{capacity}% {icon}";
-            format-icons = [" " " " " " " " " "];
-            interval = 60;
-            states = {
-              warning = 30;
-              critical = 15;
+            "sway/workspaces" = {
+              disable-scroll = true;
+              all-outputs = false;
             };
           };
+          mainBar = {
+            layer = "top";
+            position = "top";
+            height = 16;
+            output = [
+              gigabyte_4k_monitor
+              "eDP-1"
+            ];
+            modules-left = ["sway/workspaces" "mpris"];
+            modules-center = ["clock"];
+            modules-right = ["battery" "memory" "cpu" "tray"];
 
-          memory = {
-            interval = 30;
-            format = "{}%  ";
-            max-length = 10;
-          };
+            battery = {
+              format = "{capacity}% {icon}";
+              format-icons = [" " " " " " " " " "];
+              interval = 60;
+              states = {
+                warning = 30;
+                critical = 15;
+              };
+            };
 
-          tray = {
-            icon-size = 24;
-            spacing = 8;
-          };
+            memory = {
+              interval = 30;
+              format = "{}%  ";
+              max-length = 10;
+            };
 
-          "sway/workspaces" = {
-            disable-scroll = true;
-            all-outputs = false;
-          };
+            tray = {
+              icon-size = 24;
+              spacing = 8;
+            };
 
-          mpris = {
-            interval = 15;
-            format = "<span font_family=\"Victor Mono Nerd Font\"> </span>{album} · {title}";
-            on-click = "${display_album_art}/bin/display_album_art";
-          };
+            "sway/workspaces" = {
+              disable-scroll = true;
+              all-outputs = false;
+            };
 
-          cpu.format = "{usage}% ";
+            mpris = {
+              interval = 15;
+              format = "<span font_family=\"Victor Mono Nerd Font\"> </span>{album} · {title}";
+              on-click = "${display_album_art}/bin/display_album_art";
+            };
 
-          clock = {
-            format = "   {:%R}";
-            tooltip-format = "<tt><span>{calendar}</span></tt>";
-            calendar.format = {
-              days = "<span color='#ecc6d9'><b>{}</b></span>";
-              today = "<span color='#ff6699'><b><u>{}</u></b></span>";
+            cpu.format = "{usage}% ";
+
+            clock = {
+              format = "   {:%R}";
+              tooltip-format = "<tt><span>{calendar}</span></tt>";
+              calendar.format = {
+                days = "<span color='#ecc6d9'><b>{}</b></span>";
+                today = "<span color='#ff6699'><b><u>{}</u></b></span>";
+              };
             };
           };
         };
       };
-    };
 
-    wayland.windowManager.sway = {
-      enable = true;
-      extraConfig = ''
-        input * xkb_layout pl
-      '';
-      config = rec {
-        modifier = "Mod4";
-        # Use kitty as default terminal
-        terminal = "${pkgs.kitty}/bin/kitty";
-        menu = "${pkgs.fuzzel}/bin/fuzzel --show-actions";
-        gaps = {
-          inner = 12;
-          outer = 5;
-          smartGaps = true;
-          smartBorders = "no_gaps";
-        };
-        window.titlebar = false;
-        window.commands = [
-          {
-            command = "floating enable, resize set 512 512";
-            criteria = {
-              title = "imv_album_art";
-            };
-          }
-        ];
-        workspaceAutoBackAndForth = true;
-        startup = [
-          {
-            command = "systemctl --user restart kanshi";
-            always = true;
-          }
-          {
-            command = "XDG_CONFIG_HOME=/home/q/.config ${pkgs.swaynotificationcenter}/bin/swaync";
-            always = true;
-          }
-          {
-            command = "systemctl --user restart waybar";
-            always = true;
-          }
-        ];
-        keybindings = lib.mkOptionDefault {
-          # Focus window
-          "${modifier}+n" = "focus left";
-          "${modifier}+e" = "focus down";
-          "${modifier}+i" = "focus up";
-          "${modifier}+o" = "focus right";
-          # Move window
-          "${modifier}+Shift+n" = "move left";
-          "${modifier}+Shift+e" = "move down";
-          "${modifier}+Shift+i" = "move up";
-          "${modifier}+Shift+o" = "move right";
-          # Workspaces
-          "${modifier}+0" = "workspace number 10";
-          "${modifier}+Shift+0" = "move container to workspace number 10";
-          # Mode
-          "${modifier}+u" = "layout default";
-          "${modifier}+v" = "splitv";
-          "${modifier}+h" = "splith";
-
-          "${modifier}+y" = "exec ${pkgs.swaynotificationcenter}/bin/swaync-client -t -sw";
-
-          # Quick launch
-          "${modifier}+m" = "exec emacsclient -c";
-
-          "${modifier}+s" = "layout default";
-          "${modifier}+w" = "layout default";
-
-          "${modifier}+p" = "exec ${pkgs.grim}/bin/grim -g \"$(${pkgs.slurp}/bin/slurp -d)\" - | ${pkgs.wl-clipboard}/bin/wl-copy";
-          "${modifier}+l" = "exec ${lock_command}/bin/lock_command";
-        };
-        modes = {
-          resize = {
-            # colemak
-            n = "resize shrink width 50px";
-            e = "resize grow height 50px";
-            i = "resize shrink height 50px";
-            o = "resize grow width 50px";
-            # arrows
-            Left = "resize shrink width 50px";
-            Up = "resize grow height 50px";
-            Down = "resize shrink height 50px";
-            Right = "resize grow width 50px";
-            # Set %
-            Equal = "resize set height 50ppt";
-            "1" = "resize set height 10ppt";
-            "2" = "resize set height 20ppt";
-            "3" = "resize set height 30ppt";
-            "4" = "resize set height 40ppt";
-            "5" = "resize set height 50ppt";
-            "6" = "resize set height 60ppt";
-            "7" = "resize set height 70ppt";
-            "8" = "resize set height 80ppt";
-            "9" = "resize set height 90ppt";
-            Escape = "mode default";
+      wayland.windowManager.sway = {
+        enable = true;
+        extraConfig = ''
+          input * xkb_layout pl
+        '';
+        config = rec {
+          modifier = "Mod4";
+          # Use kitty as default terminal
+          terminal = "${pkgs.kitty}/bin/kitty";
+          menu = "${pkgs.fuzzel}/bin/fuzzel --show-actions";
+          gaps = {
+            inner = 12;
+            outer = 5;
+            smartGaps = true;
+            smartBorders = "no_gaps";
           };
+          window.titlebar = false;
+          window.commands = [
+            {
+              command = "floating enable, resize set 512 512";
+              criteria = {
+                title = "imv_album_art";
+              };
+            }
+          ];
+          workspaceAutoBackAndForth = true;
+          startup = [
+            {
+              command = "systemctl --user restart kanshi";
+              always = true;
+            }
+            {
+              command = "XDG_CONFIG_HOME=/home/q/.config ${pkgs.swaynotificationcenter}/bin/swaync";
+              always = true;
+            }
+            {
+              command = "systemctl --user restart waybar";
+              always = true;
+            }
+          ];
+          keybindings = lib.mkOptionDefault {
+            # Focus window
+            "${modifier}+n" = "focus left";
+            "${modifier}+e" = "focus down";
+            "${modifier}+i" = "focus up";
+            "${modifier}+o" = "focus right";
+            # Move window
+            "${modifier}+Shift+n" = "move left";
+            "${modifier}+Shift+e" = "move down";
+            "${modifier}+Shift+i" = "move up";
+            "${modifier}+Shift+o" = "move right";
+            # Workspaces
+            "${modifier}+0" = "workspace number 10";
+            "${modifier}+Shift+0" = "move container to workspace number 10";
+            # Mode
+            "${modifier}+u" = "layout default";
+            "${modifier}+v" = "splitv";
+            "${modifier}+h" = "splith";
+
+            "${modifier}+y" = "exec ${pkgs.swaynotificationcenter}/bin/swaync-client -t -sw";
+
+            # Quick launch
+            "${modifier}+m" = "exec emacsclient -c";
+
+            "${modifier}+s" = "layout default";
+            "${modifier}+w" = "layout default";
+
+            "${modifier}+p" = "exec ${pkgs.grim}/bin/grim -g \"$(${pkgs.slurp}/bin/slurp -d)\" - | ${pkgs.wl-clipboard}/bin/wl-copy";
+            "${modifier}+l" = "exec ${lock_command}/bin/lock_command";
+          };
+          modes = {
+            resize = {
+              # colemak
+              n = "resize shrink width 50px";
+              e = "resize grow height 50px";
+              i = "resize shrink height 50px";
+              o = "resize grow width 50px";
+              # arrows
+              Left = "resize shrink width 50px";
+              Up = "resize grow height 50px";
+              Down = "resize shrink height 50px";
+              Right = "resize grow width 50px";
+              # Set %
+              Equal = "resize set height 50ppt";
+              "1" = "resize set height 10ppt";
+              "2" = "resize set height 20ppt";
+              "3" = "resize set height 30ppt";
+              "4" = "resize set height 40ppt";
+              "5" = "resize set height 50ppt";
+              "6" = "resize set height 60ppt";
+              "7" = "resize set height 70ppt";
+              "8" = "resize set height 80ppt";
+              "9" = "resize set height 90ppt";
+              Escape = "mode default";
+            };
+          };
+          fonts = {
+            names = ["VictorMono Nerd Font"];
+            style = "Regular";
+            size = 16.0;
+          };
+          bars = [];
         };
-        fonts = {
-          names = ["VictorMono Nerd Font"];
-          style = "Regular";
-          size = 16.0;
-        };
-        bars = [];
       };
-    };
-  }
+    }
+  ]
