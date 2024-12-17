da8b0b876a8f0b00cb4b80f38b120ee46ddfc41e
Maksymilian Jodłowski <maksymilian.jodlowski@gmail.com>
2024-12-17 11:19:09 +0100

    :wrench: Add colon emoji support to NeoVim

---
+++ users/q/neovim/neovim/lua/custom/plugins/emojify.lua
@@ -0,0 +1,12 @@
+return {
+  {
+    'ronisbr/emojify.nvim',
+    cmd = 'Emojify',
+    config = function()
+      require('emojify').setup {
+        inlay = false,
+      }
+    end,
+    lazy = true,
+  },
+}
