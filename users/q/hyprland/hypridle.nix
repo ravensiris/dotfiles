d9993ce4ad6d1ec55c66c98b56ec9b7f147c196c
Maksymilian Jodłowski <maksymilian.jodlowski@gmail.com>
2025-01-09 00:47:58 +0100

    🕐 Change hypridle timeouts

--- users/q/hyprland/hypridle.nix
+++ users/q/hyprland/hypridle.nix
@@ -7,16 +7,17 @@ in {
     settings = {
       general = {
         after_sleep_cmd = "${pkgs.hyprland}/bin/hyprctl dispatch dpms on";
+        ignore_dbus_inhibit = false;
         lock_cmd = "${pkgs.hyprlock}/bin/hyprlock";
       };
 
       listener = [
         {
-          timeout = 900;
+          timeout = 300;
           on-timeout = "${pkgs.hyprlock}/bin/hyprlock";
         }
         {
-          timeout = 1200;
+          timeout = 600;
           on-timeout = "${pkgs.hyprland}/bin/hyprctl dispatch dpms off";
           on-resume = "${pkgs.hyprland}/bin/hyprctl dispatch dpms on";
         }
