f09a2d185dcdd10c96b57077e8572cb3751a7bdf
Maksymilian Jodłowski <maksymilian.jodlowski@gmail.com>
2025-02-17 02:52:28 +0100

    🐛 Remove global android-studio

--- hosts/gate/default.nix
+++ hosts/gate/default.nix
@@ -98,6 +98,11 @@
   nix.settings.trusted-public-keys = ["devenv.cachix.org-1:w1cLUi8dv3hnoSPGAuibQv+f9TZLr6cv/Hm9XgU50cw=" "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="];
   # nix.settings.trusted-substituters = [];
 
+  nixpkgs.config.allowUnfreePredicate = pkg:
+    builtins.elem (lib.getName pkg) [
+      "android-studio-stable"
+    ];
+
   networking.hostName = "gate";
   system.stateVersion = "24.05";
 }
--- users/q/android-studio.nix
+++ users/q/android-studio.nix
@@ -1,28 +1,10 @@
-{
-  pkgs,
-  config,
-  ...
-}: {
-  users.users."q".extraGroups = ["adbusers"];
-
-  programs.adb.enable = true;
-
-  home.packages = with pkgs; [android-studio];
-
-  home.sessionVariables = {
-    _JAVA_OPTIONS = "-Djava.util.prefs.userRoot=${config.xdg.configHome}/java";
-    ADB_VENDOR_KEY = "${config.xdg.configHome}/android";
-    ANDROID_USER_HOME = "${config.xdg.dataHome}/android";
-    ANDROID_AVD_HOME = "${config.xdg.dataHome}/android/avd";
-  };
-
+{...}: {
   home.persistence."/nix/persist/home/q".directories = [
     ".cache/Google"
     ".config/Google"
     ".local/share/Google"
 
     ".config/java"
-    ".gradle"
 
     ".config/android"
     ".local/share/android"
