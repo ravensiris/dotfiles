83817f1864ffebcc975261a5f47c0ac92858cf9b
Maksymilian Jodłowski <maksymilian.jodlowski@gmail.com>
2025-03-22 11:52:37 +0100

    🔥 Remove patches from looking-glass & kvmfr

--- hosts/default.nix
+++ hosts/default.nix
@@ -23,46 +23,6 @@
   wfetchOverlay = final: prev: {
     wfetch = wfetch.packages.${prev.system}.default;
   };
-
-  lookingGlassOverlay = final: prev: {
-    linuxPackages_latest = prev.linuxPackages_latest.extend (kfinal: kprev: {
-      kvmfr = prev.linuxPackages_latest.kvmfr.overrideAttrs (old: {
-        patches = [
-          # fix build for linux-6_10
-          (prev.fetchpatch {
-            url = "https://github.com/gnif/LookingGlass/commit/7305ce36af211220419eeab302ff28793d515df2.patch";
-            hash = "sha256-97nZsIH+jKCvSIPf1XPf3i8Wbr24almFZzMOhjhLOYk=";
-            stripLen = 1;
-          })
-
-          # securtiy patch for potential buffer overflow
-          # https://github.com/gnif/LookingGlass/issues/1133
-          (prev.fetchpatch {
-            url = "https://github.com/gnif/LookingGlass/commit/3ea37b86e38a87ee35eefb5d8fcc38b8dc8e2903.patch";
-            hash = "sha256-Kk1gN1uB86ZJA374zmzM9dwwfMZExJcix3hee7ifpp0=";
-            stripLen = 1;
-          })
-
-          (prev.fetchpatch {
-            # name = "kvmfr-6.13.patch"
-            url = "file://${./kvmfr-6.13.patch}";
-            hash = "sha256-ux3d9dO16IosLSDGxoqeRRPmEqKxXEBJKrKwRrq6TXI=";
-            stripLen = 1;
-          })
-        ];
-      });
-    });
-
-    looking-glass-client = prev.looking-glass-client.overrideAttrs (old: {
-      patches = [
-        (prev.fetchpatch {
-          url = "https://github.com/gnif/LookingGlass/commit/20972cfd9b940fddf9e7f3d2887a271d16398979.patch";
-          hash = "sha256-CqB8AmOZ4YxnEsQkyu/ZEaun6ywpSh4B7PM+MFJF0qU=";
-          stripLen = 1;
-        })
-      ];
-    });
-  };
 in {
   gate = lib.nixosSystem {
     system = "x86_64-linux";
@@ -73,7 +33,7 @@ in {
       nur.modules.nixos.default
       agenix.nixosModules.default
       unstableModule
-      {nixpkgs.overlays = [wfetchOverlay lookingGlassOverlay];}
+      {nixpkgs.overlays = [wfetchOverlay];}
       {imports = [../modules/kvmfr.nix ../modules/s3fs.nix];}
       ./gate
       home-manager.nixosModules.home-manager
