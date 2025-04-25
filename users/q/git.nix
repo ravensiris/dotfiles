e5916f918e4c677e1234818a4c265e20499d76d5
Maksymilian Jodłowski <maksymilian.jodlowski@gmail.com>
2025-04-25 02:32:31 +0200

    🔧 Use ssh-key for GitHub https cloned repos

--- users/q/git.nix
+++ users/q/git.nix
@@ -7,5 +7,12 @@
     };
     userEmail = "maksymilian.jodlowski@gmail.com";
     userName = "Maksymilian Jodłowski";
+    extraConfig = {
+      url = {
+        "git@github.com:" = {
+          insteadOf = "https://github.com/";
+        };
+      };
+    };
   };
 }
