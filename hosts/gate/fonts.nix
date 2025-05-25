19922f900220a982685dd68fa8157d49fea83509
Maksymilian Jodłowski <maksymilian.jodlowski@gmail.com>
2025-05-25 22:29:19 +0200

    🖊️ Use all the nerdy fonts

--- hosts/gate/fonts.nix
+++ hosts/gate/fonts.nix
@@ -1,16 +1,17 @@
 {pkgs, ...}: {
   fonts.fontDir.enable = true;
-  fonts.packages = with pkgs; [
-    migu
-    baekmuk-ttf
-    nanum
-    noto-fonts-emoji
-    twemoji-color-font
-    openmoji-color
-    twitter-color-emoji
-    nerdfonts
-    mno16
-  ];
+  fonts.packages = with pkgs;
+    [
+      migu
+      baekmuk-ttf
+      nanum
+      noto-fonts-emoji
+      twemoji-color-font
+      openmoji-color
+      twitter-color-emoji
+      mno16
+    ]
+    ++ builtins.filter lib.attrsets.isDerivation (builtins.attrValues pkgs.nerd-fonts);
 
   fonts.fontconfig = {
     defaultFonts = {
