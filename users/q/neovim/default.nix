1787f67c0cef79c6d4c3f1ef15808c83e9ea39c9
Maksymilian Jodłowski <maksymilian.jodlowski@gmail.com>
2025-05-25 22:50:16 +0200

    📦 Use nixpkgs protols

--- users/q/neovim/default.nix
+++ users/q/neovim/default.nix
@@ -3,21 +3,7 @@
   pkgs,
   config,
   ...
-}: let
-  protols = pkgs.rustPlatform.buildRustPackage rec {
-    pname = "protols";
-    version = "0.7.0";
-
-    src = pkgs.fetchFromGitHub {
-      owner = "coder3101";
-      repo = pname;
-      rev = version;
-      hash = "sha256-yK0KgwHR+nXJu1TLXMhrpXR/4gFM7JVSqjtkUinESSE=";
-    };
-    cargoHash = "sha256-2Cxf9r3SaMlOyr0TSczigtK1ZSNEzeaNzbzYMhCEc4M=";
-    doCheck = false;
-  };
-in {
+}: {
   xdg = {
     enable = true;
     configFile."nvim" = {
