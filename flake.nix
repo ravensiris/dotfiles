7ac72ea20c4a895d01881cd480068fa74552c74f
Maksymilian Jodłowski <maksymilian.jodlowski@gmail.com>
2025-12-01 20:23:28 +0100

    ❄️ Update to 25.11

--- flake.nix
+++ flake.nix
@@ -2,10 +2,10 @@
   description = "ravensiris' dotfiles";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.05";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.11";
     nixpkgs-unstable.url = "github:nixos/nixpkgs/nixpkgs-unstable";
 
-    home-manager.url = "github:nix-community/home-manager/release-25.05";
+    home-manager.url = "github:nix-community/home-manager/release-25.11";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
 
     impermanence.url = "github:nix-community/impermanence";
